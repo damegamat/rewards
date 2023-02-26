b31f476d3c5724ba28917a807624a24c2bd0cb32
Darek <dar.lewandowski93@gmail.com>
2023-02-26 12:31:57 +0100

    fetch transactions and create table

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,7 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { fetchTransactions } from '../../services'
 
 function Dashboard() {
-  return <div className='Dashboard'>Dashboard</div>
+  const [transactions, setTransactions] = useState([])
+
+  useEffect(() => {
+    ;(async () => {
+      const newTransactions = await fetchTransactions()
+      setTransactions(newTransactions)
+    })()
+  }, [])
+
+  return (
+    <div className='Dashboard'>
+      <h1>Dashboard</h1>
+      <h2>Transactions</h2>
+      <div>
+        <table>
+          <thead>
+            <th>Number</th>
+            <th>User Name</th>
+            <th>Product Name</th>
+            <th>Date</th>
+            <th>Price</th>
+          </thead>
+          <tbody>
+            {transactions.map(({ date, user, item, transactionId }, i) => (
+              <tr key={transactionId}>
+                <td>{i}</td>
+                <td>{user.userFullName}</td>
+                <td>{item.itemName}</td>
+                <td>{new Date(date).toLocaleDateString()}</td>
+                <td>{item.itemPrice}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  )
 }
 
 export default Dashboard
